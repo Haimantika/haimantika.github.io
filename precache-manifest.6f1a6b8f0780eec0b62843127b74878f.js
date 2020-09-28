a59599bed227b1a578b110f2e543889c95c1026f
Haimantika Mitra <haimantikamitra@gmail.com>
2020-09-28 16:55:02 +0530

    Updates

---
+++ precache-manifest.6f1a6b8f0780eec0b62843127b74878f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b093a2375e4e5aea3c7b6aa412897cbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "128b5521cf95e2bb7970",
+    "url": "/static/css/3.9998556b.chunk.css"
+  },
+  {
+    "revision": "3550d313b05d9f671f0d",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "eb265c289e5642a35d4d",
+    "url": "/static/css/main.da7c0cf5.chunk.css"
+  },
+  {
+    "revision": "89051d44a4d625b98b6b",
+    "url": "/static/js/2.4809646c.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/static/js/2.4809646c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "128b5521cf95e2bb7970",
+    "url": "/static/js/3.d2046ea3.chunk.js"
+  },
+  {
+    "revision": "3550d313b05d9f671f0d",
+    "url": "/static/js/4.93a6ca57.chunk.js"
+  },
+  {
+    "revision": "eb265c289e5642a35d4d",
+    "url": "/static/js/main.c0dab62d.chunk.js"
+  },
+  {
+    "revision": "a116572dae2b524f8dde",
+    "url": "/static/js/runtime-main.fad6ea9a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "11c96e281d1c3da5088f8e0c84799ef9",
+    "url": "/static/media/Msbuild.11c96e28.jpg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "8bee95d29d52b27dbfd0856c36e50991",
+    "url": "/static/media/ga.8bee95d2.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "44a860a311c1fc71e0d0cf4af28dc53b",
+    "url": "/static/media/msplogo.44a860a3.png"
+  },
+  {
+    "revision": "119d33adf223991df97937c389e88e86",
+    "url": "/static/media/sihlogo.119d33ad.jpg"
+  },
+  {
+    "revision": "5300adf0d8a959d329567bc89a3b410f",
+    "url": "/static/media/swologo.5300adf0.png"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "7b55cb57b78401deb5d7c4d069c1d9bb",
+    "url": "/static/media/w1.7b55cb57.png"
+  },
+  {
+    "revision": "4196b47d00a891f01779911575ee5e11",
+    "url": "/static/media/w2.4196b47d.jpg"
+  },
+  {
+    "revision": "dfc2ee04512b473a407b35e12b6192ba",
+    "url": "/static/media/womanot.dfc2ee04.jpg"
+  },
+  {
+    "revision": "5270dfc0b8f8efd5e224a8d6f01ada47",
+    "url": "/static/media/wtmlogo.5270dfc0.png"
+  }
+]);
